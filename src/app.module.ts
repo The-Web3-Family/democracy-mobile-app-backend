9c0daf6ce079c66e46e1151dd6bc2f741562df3d
Gideon Moses <gideon.moses@miatech.pro>
2024-11-29 23:13:10 +0100

    bills cron added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BillModule } from './bill/bill.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ScheduleModule } from '@nestjs/schedule';
+
 
 @Module({
-  imports: [],
+  imports: [
+    BillModule, 
+    PrismaModule,
+    ScheduleModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
